6c7d8133572e8cab40437057386fce8274110b11
jarad <niemi@iastate.edu>
2016-04-09 21:31:23 -0500

    Create cumulative mile summary

--- server.R
+++ server.R
@@ -1,43 +1,37 @@
 library(shiny)
 
-d = read.csv("data/id.csv")
+cards_per_mile = 5
+
+source("read_cards.R")
+
+total_miles = d %>%
+  group_by(week) %>%
+  summarize(cards = n()) %>%
+  arrange(week) %>%
+  mutate(cumulative_cards = cumsum(cards),
+         cumulative_miles = cards_per_mile*cumulative_cards)
+
+milestones = d %>%
+  group_by(id, week) %>%
+  summarize(total_miles = cards_per_mile*n()) %>%
+  group_by(total_miles) %>%
+  summarize(this_week = n()) %>%
+  arrange(total_miles) %>%
+  mutate(cumulative = cumsum(this_week))
+  
 
 shinyServer(function(input, output) {
   
   # display 10 rows initially
-  output$ex1 <- DT::renderDataTable(
-    DT::datatable(d, options = list(pageLength = 25))
+  output$ex1 = DT::renderDataTable(
+    DT::datatable(read.csv("data/id.csv"), options = list(pageLength = 25))
   )
   
-  # -1 means no pagination; the 2nd element contains menu labels
-  output$ex2 <- DT::renderDataTable(
-    DT::datatable(
-      d, options = list(
-        lengthMenu = list(c(5, 15, -1), c('5', '15', 'All')),
-        pageLength = 15
-      )
-    )
+  output$total_miles = DT::renderDataTable(
+    DT::datatable(total_miles)
   )
   
-  # you can also use paging = FALSE to disable pagination
-  output$ex3 <- DT::renderDataTable(
-    DT::datatable(d, options = list(paging = FALSE))
+  output$milestones = DT::renderDataTable(
+    DT::datatable(milestones)
   )
-  
-  # turn off filtering (no searching boxes)
-  output$ex4 <- DT::renderDataTable(
-    DT::datatable(d, options = list(searching = FALSE))
-  )
-  
-  # write literal JS code in JS()
-  output$ex5 <- DT::renderDataTable(DT::datatable(
-    d,
-    options = list(rowCallback = DT::JS(
-      'function(row, data) {
-      // Bold cells for those >= 5 in the first column
-      if (parseFloat(data[1]) >= 5.0)
-      $("td:eq(1)", row).css("font-weight", "bold");
-}'
-    ))
-  ))
 })
