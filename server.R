9b835e18859a71cacf3e4bab94b784b9e0219736
jarad <niemi@iastate.edu>
2016-04-09 20:37:38 -0500

    Create shiny app
    Copy from http://shiny.rstudio.com/gallery/datatables-options.html

---
+++ server.R
@@ -0,0 +1,43 @@
+library(shiny)
+
+d = read.csv("data/id.csv")
+
+shinyServer(function(input, output) {
+  
+  # display 10 rows initially
+  output$ex1 <- DT::renderDataTable(
+    DT::datatable(d, options = list(pageLength = 25))
+  )
+  
+  # -1 means no pagination; the 2nd element contains menu labels
+  output$ex2 <- DT::renderDataTable(
+    DT::datatable(
+      d, options = list(
+        lengthMenu = list(c(5, 15, -1), c('5', '15', 'All')),
+        pageLength = 15
+      )
+    )
+  )
+  
+  # you can also use paging = FALSE to disable pagination
+  output$ex3 <- DT::renderDataTable(
+    DT::datatable(d, options = list(paging = FALSE))
+  )
+  
+  # turn off filtering (no searching boxes)
+  output$ex4 <- DT::renderDataTable(
+    DT::datatable(d, options = list(searching = FALSE))
+  )
+  
+  # write literal JS code in JS()
+  output$ex5 <- DT::renderDataTable(DT::datatable(
+    d,
+    options = list(rowCallback = DT::JS(
+      'function(row, data) {
+      // Bold cells for those >= 5 in the first column
+      if (parseFloat(data[1]) >= 5.0)
+      $("td:eq(1)", row).css("font-weight", "bold");
+}'
+    ))
+  ))
+})
