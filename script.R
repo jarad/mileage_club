de00360f390f590b4879e62ee8180180e147468c
Camille Niemi <camilleniemi@gmail.com>
2016-04-16 15:24:02 -0500

    Add commit about git

--- script.R
+++ script.R
@@ -10,6 +10,9 @@ ids = readr::read_csv('data/id.csv')
 # If you need to find a student's name or ID
 find_name(ids)
 
+# If you want to learn to use git, you can commit the changes to days.csv and the added files.
+
 # 3) Now read in the cards and create the summary
 cards = read_cards()
 summary(cards)
+
