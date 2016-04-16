3a4918f2c8ce5b5e3cc43bc2bba63098750a6cfb
jarad <niemi@iastate.edu>
2016-04-16 14:35:08 -0500

    Provide comments in the script

--- script.R
+++ script.R
@@ -1,8 +1,16 @@
 library(dplyr)
 library(mileageClub)
 
-cards = read_cards()
 ids = readr::read_csv('data/id.csv')
 
-summary(cards, ids)
+# 1) Update days.csv
+# 2) Create a new YYYYMMDD.csv file with 'id' as the header then all the IDs of cards that
+#    were turned in that day. Save this file in the data/cards/ directory. Easiest way may
+#    be to copy a previous file and put in the IDs. 
 
+# If you need to find a student's name or ID
+find_name(ids)
+
+# 3) Now read in the cards and create the summary
+cards = read_cards()
+summary(cards)
