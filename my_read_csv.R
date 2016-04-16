717c789f076bb73ac0f8e5882587139df6dc00ed
jarad <niemi@iastate.edu>
2016-04-16 12:59:46 -0500

    Delete R functions that are now in the mileageClub package

--- my_read_csv.R
+++
@@ -1,8 +0,0 @@
-library(stringr)
-library(readr)
-
-my_read_csv = function(f) {
-  d = read_csv(f)
-  d$date = as.Date(str_extract(f, '[0-9]{8}'), format="%Y%m%d")
-  return(d)
-}
--- ui.R
+++
@@ -1,8 +0,0 @@
-library(shiny)
-
-shinyUI(navbarPage(
-  title = 'Mary Greeley Mileage Club',
-  tabPanel('Find student',     DT::dataTableOutput('ex1')),
-  tabPanel('Total Miles',          DT::dataTableOutput('total_miles')),
-  tabPanel('Milestones',          DT::dataTableOutput('milestones'))
-))
