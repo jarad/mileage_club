6ab676fb76eed2fd6dd5a0d7722e33ed70603fb8
Camille Niemi <camilleniemi@gmail.com>
2016-04-25 21:07:29 -0500

    Add students per class

--- script.R
+++ script.R
@@ -2,6 +2,13 @@ library(mileageClub)
 
 ids = readr::read_csv('data/id.csv'); ids$id_text = NULL
 
+# # students per class
+# library(dplyr)
+# tmp = ids %>%
+#   group_by(grade,teacher,grade_teacher) %>%
+#   summarize(n=n())
+# write.csv(tmp, file='students_per_class.csv')
+
 # 1) Update days.csv
 # 2) Create a new YYYYMMDD.csv file with 'id' as the header then all the IDs of cards that
 #    were turned in that day. Save this file in the data/cards/ directory. Easiest way may
@@ -19,7 +26,7 @@ summary(cards, ids)
 # 4) Create 10-mile Certificates
 #    a) Run summary line, click open in browser
 #    b) Save appropriate certificate file on 'Certificate' tab
-#    c) Click on certificates/10mile.dotx and say 'No'
+#    c) In R, right bottom corner, Click on mileage_club/certificates/10mile.dotx and say 'No'
 #    d) Click 'Mailings' and 'Select Receipts > Use an existing list...'
 #    e) Open saved file from a), most likely in Downloads
 #    f) Preview Results, Click Finish & Merge, print documents, select All and click ok
